6767fd24ccb3823e3cca3ded17eea13b70682c25
Pedro Henestroza <phenestroza@gmail.com>
2025-10-04 15:58:01 -0300

    Improved three column block

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,12 @@ import content from "@/content/homepage.json";
 
 export default function Home() {
 
+  const hero = content.components[0];
+
   return (
     <div className="main">
-      <Hero title='Mueblería' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl eget ultricies ultricies, nunc nisi aliquet nunc, eu tincidunt nisi nisl eu nisl. Aliquam erat volutpat.' image='https://picsum.photos/2000/3000' cta={{text: 'Ver Catalogo', link: '/catalogo', ariaLabel: 'Ver Catalogo'}} />
+      <Hero title={hero.props.title} description={hero.props.description} image={hero.props.image} cta={hero.props.cta} />
+      <div className='splitter w-full h-[10px] bg-gray-300'></div>
       <ThreeColumnBlock columns={content.components[1].props.columns} />
     </div>
   );
--- src/components/ThreeColumnBlock/ThreeColumnBlock.js
+++ src/components/ThreeColumnBlock/ThreeColumnBlock.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-
-// Expects a columns object with 3 column objects inside. 
+// Expects a columns object with 3 column objects inside.
 // Each column should have:
 // text: string
 // image: string
 // id: int
-const ThreeColumnBlock = ({columns}) => {
+const ThreeColumnBlock = ({ columns }) => {
   return (
-    <section className='threeColumnBlock'>
-        <div className='threeColumnBlock__container flex flex-col md:flex-row'>
-            {columns.map((column) => {
-                return(
-                    <div className='threeColumnBlock__column w-full md:w-[33%] h-[300px] md:h-[500px] relative' key={column.id}>
-                        <div className='threeColumnBlock__image z-1'>
-                            <Image fill src={column.image} alt={column.text} size={'100vw'} className='z-1 object-cover'></Image>
-                        </div>
-                        <div className='relative threeColumnBlock__content px-8 h-full flex items-center justify-center z-2'>
-                            <h2>{column.text}</h2>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    </section>
-  )
-}
+    <section className="threeColumnBlock">
+      <div className="threeColumnBlock__container flex flex-col md:flex-row overflow-hidden">
+        {columns.map((column) => {
+          return (
+            <div className="threeColumnBlock__column group w-full md:w-[33.33%] h-[300px] md:h-[500px] relative" key={column.id}>
+              <div className="threeColumnBlock__image z-1">
+                <Image fill src={column.image} alt={column.text} size={"100vw"} className="z-1 object-cover group-hover:scale-110 transform-duration-300 transition-transform"></Image>
+              </div>
+              <div className="threeColumnBlock__overlay z-2 bg-gray-500 opacity-30 transition-opacity duration-300 absolute w-full h-full top-0 left-0 right-0 bottom-0"></div>
 
-export default ThreeColumnBlock
+              <div className="relative threeColumnBlock__content px-8 h-full flex items-center justify-center z-3">
+                <h2>{column.text}</h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ThreeColumnBlock;
